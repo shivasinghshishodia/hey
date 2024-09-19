0a448d44f48b54aa36b2e7bf4979cc1b0493cc2f
shivasinghshishodia <130183426+shivasinghshishodia@users.noreply.github.com>
2024-09-19 21:40:45 +0530

    Update views.py

--- polling system/pollme/views.py
+++ polling system/pollme/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 def about(request):
     return render(request, 'about.html')
